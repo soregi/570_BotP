a1853884814687d8882c7e31c8272834412530d2
soregix <bhlee0327@naver.com>
2020-05-20 17:53:28 +0900

    Initial

---
+++ common/defines/570_defines.lua
@@ -0,0 +1,2 @@
+NDefines.NTechnology.DONT_EXECUTE_TECH_BEFORE = 570
+NDefines.NTechnology.IDEAL_YEAR_LEVEL_0 = 500
---
+++ descriptor.mod
@@ -0,0 +1,4 @@
+name = "570: Birth of the Prophet"
+path = "mod/qara"
+picture = "pci.png"
+user_dir = "qara"
